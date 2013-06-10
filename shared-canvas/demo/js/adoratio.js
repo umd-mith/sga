0144cca8d980d75e3e407377da4005f2791e3a37
Raffaele Viglianti <raffaeleviglinati@gmail.com>
2013-06-10 16:00:49 -0400

    updated to latest v

--- shared-canvas/demo/js/adoratio.js
+++ shared-canvas/demo/js/adoratio.js
@@ -3,9 +3,11 @@
   (function($) {
     return window.adoratio = function(c, baseURL, map) {
       return function(data) {
-        var HTMLcontainer, dj, getImgCenter, getImgPosition, getImgTiles, po, startZoom;
+        var dj, getImgCenter, getImgPosition, getImgTiles, po, startZoom;
 
-        HTMLcontainer = c.get(0);
+        data.levels = parseInt(data.levels);
+        data.height = parseInt(data.height);
+        data.width = parseInt(data.width);
         dj = {};
         dj.tile2long = function(x, z) {
           return x / Math.pow(2, z) * 360 - 180;
@@ -212,8 +214,8 @@
         };
         po = org.polymaps;
         map.add(po.dblclick()).add(dj.drag()).add(po.wheel());
-        startZoom = (Math.ceil(parseInt(data.levels) + Math.log(HTMLcontainer.clientWidth) / Math.log(2) - Math.log(data.width) / Math.log(2))) - 1;
-        map.zoomRange([startZoom, parseInt(data.levels)]).zoom(startZoom);
+        startZoom = data.levels + Math.log(c.clientWidth) / Math.log(2) - Math.log(data.width) / Math.log(2);
+        map.zoomRange([startZoom, data.levels]).zoom(startZoom);
         dj.cc = map.center();
         map.center(getImgCenter(data.levels, startZoom));
         map.on('drag', function() {
@@ -221,7 +223,7 @@
 
           p = getImgPosition(data.levels, map.zoom());
           currentImgSize = dj.getImgDimensions(map.zoom());
-          if (p.topLeft.x + currentImgSize.w > 20 && p.bottomRight.y > 20 && p.bottomRight.x - currentImgSize.w < HTMLcontainer.clientWidth - 20 && p.topLeft.y < HTMLcontainer.clientHeight - 20) {
+          if (p.topLeft.x + currentImgSize.w > 20 && p.bottomRight.y > 20 && p.bottomRight.x - currentImgSize.w < c.clientWidth - 20 && p.topLeft.y < c.clientHeight - 20) {
             return 0;
           } else {
             return map.center(dj.cc);
