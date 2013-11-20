54a7589740c47dbe4320b4d61fca25d0e53b2d2e
raffazizzi <raffaeleviglianti@gmail.com>
2013-11-20 17:00:49 -0500

    new version for new backbone app

--- shared-canvas/demo/js/main.js
+++ shared-canvas/demo/js/main.js
@@ -1,25 +1,6 @@
 (function ($) { 
-  MITHgrid.config.noTimeouts = true;
+  'use strict';
 
-  // UI fixes
-  $("#collapse-one").collapse("hide");
+  SGASharedCanvas.Application.start();
 
-  // EXAMPLE SGA Shared Canvas SETUP CODE
-
-  var builder = SGA.Reader.Application.SharedCanvas.builder();
-
-  if($.fn.popover != null) {
-    $("#metadata").popover({
-      title: "About this Manifest",
-      content: function() {
-        return "<p>We'll build up a list of items from the manifest.</p>";
-      },
-      html: true,
-      animation: true
-    });
-  }
-
-  builder.onManifest(
-    $('*[data-manifest]').attr('data-manifest')
-  );
 })(jQuery);
