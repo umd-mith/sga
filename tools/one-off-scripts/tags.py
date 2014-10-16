ee219499333a09051e297d79227894ec0b3e73d0
Ed Summers <ehs@pobox.com>
2014-10-15 21:51:57 -0400

    html output

--- tools/one-off-scripts/tags.py
+++ tools/one-off-scripts/tags.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
 """
-A crazy little script to summarize what tags are used in the SGA TEI data.
+A crazy little script to summarize what tags are used in the SGA TEI data
+and write it out as HTML.
 """
 
 import os
 import re
+import sys
 import xml.etree.ElementTree as etree
 
 class XmlSum():
 
     def __init__(self):
+        self.doc_count = 0
         self.s = {}
 
     def summarize(self, path):
@@ -18,12 +21,15 @@ class XmlSum():
         self.add(doc.getroot())
 
     def add(self, e):
+        self.doc_count += 1
         ns, tag = re.match("(?:{(.+)})(.+)", e.tag).groups()
         self.tally(ns)
         self.tally(ns, tag)
-        for name, value in e.attrib.items():
-            self.tally(ns, tag, name)
-            self.tally(ns, tag, name, value)
+        for attr_name, attr_value in e.attrib.items():
+            # attribute strings can be space delimited
+            for a in attr_name.split(' '):
+                self.tally(ns, tag, a)
+                self.tally(ns, tag, a, attr_value)
         for child in e:
             self.add(child)
     
@@ -35,14 +41,101 @@ class XmlSum():
             d = d[k]
         d["___count"] = d["___count"] + 1
 
-    def report(self):
-        namespaces = []
+    def text(self):
         for ns in self._keys(self.s):
             print "%s [%s]" % (ns, self.s[ns]["___count"])
             for tag in self._keys(self.s[ns]):
                 print " %s [%s]" % (tag, self.s[ns][tag]["___count"])
                 for attribute in self._keys(self.s[ns][tag]):
                     print "  %s [%s]" % (attribute, self.s[ns][tag][attribute]["___count"])
+
+    def html(self):
+        print '''
+<!doctype html>
+<html>
+  <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <style>
+      body {
+        font-size: 14pt;
+      }
+      ul {
+        list-style-type: none;
+      }
+      ul.attributes {
+        list-style-type: disc;
+        font-size: smaller;
+      }
+      .count {
+        font-size: smaller;
+        font-style: italic;
+        font-color: gray;
+      }
+      .namespace {
+        font-size: larger;
+        margin-top: 20px;
+      }
+      .attributes {
+        display: none;
+      }
+      .attribute {
+        font-style: italic;
+      }
+      .glyphicon {
+        color: #444444;
+      }
+    </style>
+    </script>
+  </head>
+  <body>
+  <div class="container">
+  <h1>XML Summary: %s files</h1>
+  <ul class="namespaces">
+''' % c(self.doc_count)
+        for ns in self._keys(self.s):
+            print '  <li><span class="glyphicon glyphicon-plus"></span> <span class="namespace">%s</span> <span class="count">%s</span>' % (ns, c(self.s[ns]["___count"]))
+            print '    <ul class="elements">'
+            for tag in self._keys(self.s[ns]):
+                if ns == "http://www.tei-c.org/ns/1.0":
+                    e = '<a href="http://www.tei-c.org/release/doc/tei-p5-doc/en/html/ref-%s.html">&lt;%s&gt;</a>' % (tag, tag)
+                else:
+                    e = '&lt;%s&gt;' % tag
+                print '      <li><span class="glyphicon glyphicon-plus"></span> <span class="element">%s</span> <span class="count">%s</span>' % (e, c(self.s[ns][tag]["___count"]))
+                print '        <ul class="attributes">'
+                for attribute in self._keys(self.s[ns][tag]):
+                    print '          <li><span class="attribute">%s</span> <span class="count">%s</span>' % (attribute, c(self.s[ns][tag][attribute]["___count"]))
+                print '        </ul>'
+            print '    </ul>'
+        print '''
+</ul>
+</div>
+<script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script>
+  $(function() {
+    $(".glyphicon").click(function(event) {
+      if ($(this).hasClass('glyphicon-plus')) {
+        $(this).removeClass('glyphicon-plus');
+        $(this).addClass('glyphicon-minus');
+      } else {
+        $(this).removeClass('glyphicon-minus');
+        $(this).addClass('glyphicon-plus');
+      }
+      $(this).parent().find("ul").each(function(i, ul) {
+        ul = $(ul);
+        if (ul.is(":visible")) {
+          ul.hide();
+        } else {
+          ul.show();
+        }
+      });
+    });
+  });
+</script>
+</body>
+</html>
+'''
+        
 
     def _keys(self, d):
         keys = d.keys()
@@ -62,9 +155,13 @@ def main():
             try:
                 xs.summarize(path)
             except Exception as e:
-                print "%s: %s" % (path, e)
+                sys.stderr.write("%s: %s\n" % (path, e))
 
-    xs.report()
+    xs.html()
+
+def c(n):
+    "1234 -> 1,234"
+    return format(n, ",d")
 
 if __name__ == "__main__":
     main()
