62c3253bb0134e165c95604e947836a5556380d1
Ed Summers <ehs@pobox.com>
2014-10-28 16:42:44 -0400

    simplified use of grunt to create a single app.min.js file ; may have to revisit or add source maps to make dev easier

--- site/shared-canvas/Gruntfile.js
+++ site/shared-canvas/Gruntfile.js
@@ -17,32 +17,8 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      dist: {
-        options: {
-          mangle: false,
-          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'   
-        },   
-        files: { 'dist/<%= pkg.name %>.min.js': [ 'dist/<%= pkg.name %>.js' ] }
-      }
-    },
 
-    concat: {
-      bower_js: {
-        options: {
-          separator: ';'
-        },
-        src: ['bower_components/modernizr/modernizr.js',
-              'bower_components/jquery/jquery.min.js',
-              'bower_components/jquery-ui/ui/minified/jquery-ui.min.js',
-              'bower_components/bootstrap/dist/js/bootstrap.min.js',
-              'lib/vendor/google-prettify.js',
-              'bower_components/underscore/underscore.js',
-              'bower_components/backbone/backbone.js'], 
-        dest: 'demo/js/bower_components.js'
-      }
-    },
+    pkg: grunt.file.readJSON('package.json'),
 
     coffee: {
       compileJoined: {
@@ -50,13 +26,14 @@ module.exports = function(grunt) {
           join: true
         },
         files: { 
-          'dist/<%= pkg.name %>.js': ['src/intro.coffee',
-                                      'src/utils.coffee',
-                                      'src/data.coffee',
-                                      //'src/application.coffee',
-                                      'src/component.coffee',
-                                      'src/view.coffee',
-                                      'src/router.coffee']
+          'dist/<%= pkg.name %>.js': [
+            'src/intro.coffee',
+            'src/utils.coffee',
+            'src/data.coffee',
+            'src/component.coffee',
+            'src/view.coffee',
+            'src/router.coffee'
+          ]
         }
       }
     },
@@ -64,6 +41,55 @@ module.exports = function(grunt) {
     less: {
       dev: {
         files:{'demo/css/main.css': 'less/main.less' }
+      }
+    },
+
+    concat: {
+      js: {
+        options: {
+          separator: ';'
+        },
+        src: [
+          'bower_components/modernizr/modernizr.js',
+          'bower_components/jquery/jquery.min.js',
+          'bower_components/jquery-ui/ui/minified/jquery-ui.min.js',
+          'bower_components/bootstrap/dist/js/bootstrap.min.js',
+          'lib/vendor/google-prettify.js',
+          'bower_components/underscore/underscore.js',
+          'bower_components/backbone/backbone.js',
+          'dist/<%= pkg.name %>.js'
+        ], 
+        dest: 'dist/app.js'
+      }
+    },
+
+    uglify: {
+      dist: {
+        options: {
+          mangle: false,
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'   
+        },   
+        files: {
+          'dist/app.min.js': [ 
+            'dist/app.js' 
+          ]
+        }
+      }
+    },
+
+    copy: {
+      install: {
+        files: [
+          {
+            expand: true, 
+            cwd: 'bower_components/font-awesome/font/', 
+            src: ['**'], dest: 'demo/font/'
+          },
+          {
+            src: 'dist/app.min.js',
+            dest: 'demo/js/app.min.js'
+          }
+        ]
       }
     },
 
@@ -89,44 +115,42 @@ module.exports = function(grunt) {
         }
       },
       proxies: [
-            {
-                context: '/adore-djatoka',
-                host: 'tiles2.bodleian.ox.ac.uk',
-                port: '8080',
-                changeOrigin: true,
-                xforward: false
-            }
-          ]
+        {
+          context: '/adore-djatoka',
+          host: 'tiles2.bodleian.ox.ac.uk',
+          port: '8080',
+          changeOrigin: true,
+          xforward: false
+        }
+      ]
     },
 
     watch: {
       scripts: {
         files: ['src/*.coffee', 'less/*.less'],
-        // Not uglifying, since watch is supposed to be used for development
-        tasks: ['concat:bower_js', 'coffee', 'less'], 
+        tasks: ['coffee', 'less', 'concat:js', 'uglify', 'copy:install'], 
         options: {
           livereload: true
         }
-      }
-    },
-
-    bower: { install: true },
-
-    copy: {
-      install: {
-        files: [{
-          expand: true, 
-          cwd: 'bower_components/font-awesome/font/', 
-          src: ['**'], dest: 'demo/font/'
-        }]
       }
     }
 
   });
 
+  grunt.registerTask('default', [
+    'install-dependencies', 
+    'bower', 
+    'coffee', 
+    'concat:js', 
+    'uglify', 
+    'less', 
+    'copy:install'
+  ]);
 
-  // Default task(s).
-  grunt.registerTask('default', ['concat:bower_js', 'coffee', 'uglify', 'less']);
-  grunt.registerTask('run', ['configureProxies', 'connect:server', 'watch']);
-  grunt.registerTask('install', ['install-dependencies', 'bower', 'copy:install']);
+  grunt.registerTask('run', [
+    'configureProxies', 
+    'connect:server', 
+    'watch'
+  ]);
+
 }
