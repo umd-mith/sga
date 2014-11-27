1ee14ef1eda91e3a34ab35539b93d9ecc4e83db1
raffazizzi <raffaeleviglianti@gmail.com>
2014-11-26 20:04:05 -0500

    passing on search service URL

--- site/shared-canvas/demo/js/main.js
+++ site/shared-canvas/demo/js/main.js
@@ -3,6 +3,6 @@
 
   var manifestURL = $("#SGASharedCanvasViewer").data("manifest");
 
-  var sc = new SGASharedCanvas.Application({"manifest":manifestURL});
+  var sc = new SGASharedCanvas.Application({"manifest":manifestURL, "searchService":"http://107.20.241.32/annotate?"});
 
 })(jQuery);
