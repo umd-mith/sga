8dcf77d3e101c7bc6317080933cfe71140f3531a
raffazizzi <raffaeleviglianti@gmail.com>
2015-07-16 15:59:41 -0400

    adding subdirectory to path

--- site/search/service/flask/sga_search/views.py
+++ site/search/service/flask/sga_search/views.py
@@ -163,7 +163,8 @@ def annotate():
     
     def do_annotation(s, f, q):
         # This will probably stay hardcoded
-        TEI_data = "http://shelleygodwinarchive.org/tei/ox/"
+        # TEI_data = "http://shelleygodwinarchive.org/tei/ox/"
+        TEI_data = "http://localhost:8000/tei/ox/"
         format = "jsonld"
         hl_simple_pre = '_#_'
         hl_simple_post = '_#_'
