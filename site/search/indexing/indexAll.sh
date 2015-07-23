cefcfb637a64380cdbaec6eee5acdbe66e7138d5
raffazizzi <raffaeleviglianti@gmail.com>
2015-07-23 10:25:09 -0400

    simple script to index all physical sequence tei files

---
+++ site/search/indexing/indexAll.sh
@@ -0,0 +1,8 @@
+python tei-to-solr.py ../../../data/tei/ox/ox-ms_shelley_e1.xml
+python tei-to-solr.py ../../../data/tei/ox/ox-ms_shelley_e2.xml 
+python tei-to-solr.py ../../../data/tei/ox/ox-ms_shelley_e3.xml 
+python tei-to-solr.py ../../../data/tei/ox/ox-frankenstein_notebook_a.xml
+python tei-to-solr.py ../../../data/tei/ox/ox-frankenstein_notebook_b.xml
+python tei-to-solr.py ../../../data/tei/ox/ox-frankenstein_notebook_c1.xml
+python tei-to-solr.py ../../../data/tei/ox/ox-frankenstein_notebook_c2.xml
+
