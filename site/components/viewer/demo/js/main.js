9452c70911dbb966265e7002b2c3f0a73cc317ff
raffazizzi <raffaeleviglianti@gmail.com>
2015-07-29 14:50:35 -0400

    cleaned up unnecessary components after jekyll integration

--- site/components/viewer/demo/js/main.js
+++
@@ -1,8 +0,0 @@
-(function ($) { 
-  'use strict';
-
-  var manifestURL = $("#SGASharedCanvasViewer").data("manifest");
-
-  var sc = new SGASharedCanvas.Application({"manifest":manifestURL, "searchService":"http://107.20.241.32/annotate?"});
-
-})(jQuery);
