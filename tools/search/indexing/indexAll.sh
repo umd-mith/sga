366496bbf3a841f54d4458dfc03873e77dabc175
raffazizzi <raffaeleviglianti@gmail.com>
2016-02-15 11:47:09 -0500

    Indexing new manuscript files

--- tools/search/indexing/indexAll.sh
+++ tools/search/indexing/indexAll.sh
@@ -1,8 +1,6 @@
 python tei-to-solr.py ../../../data/tei/ox/ox-ms_shelley_e1.xml
 python tei-to-solr.py ../../../data/tei/ox/ox-ms_shelley_e2.xml 
 python tei-to-solr.py ../../../data/tei/ox/ox-ms_shelley_e3.xml 
-python tei-to-solr.py ../../../data/tei/ox/ox-frankenstein_notebook_a.xml
-python tei-to-solr.py ../../../data/tei/ox/ox-frankenstein_notebook_b.xml
-python tei-to-solr.py ../../../data/tei/ox/ox-frankenstein_notebook_c1.xml
-python tei-to-solr.py ../../../data/tei/ox/ox-frankenstein_notebook_c2.xml
-
+python tei-to-solr.py ../../../data/tei/ox/ox-ms_abinger_c56.xml
+python tei-to-solr.py ../../../data/tei/ox/ox-ms_abinger_c57.xml
+python tei-to-solr.py ../../../data/tei/ox/ox-ms_abinger_c58.xml
