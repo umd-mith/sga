d6681d6063ce9f01f65e1de1cf6db6b905d9492b
raffazizzi <raffaeleviglianti@gmail.com>
2017-02-16 11:02:19 -0500

    Fixed typo

--- tools/search/indexing/indexAll.sh
+++ tools/search/indexing/indexAll.sh
@@ -4,4 +4,4 @@ python tei-to-solr.py ../../../data/tei/ox/ox-ms_shelley_e3.xml
 python tei-to-solr.py ../../../data/tei/ox/ox-ms_abinger_c56.xml
 python tei-to-solr.py ../../../data/tei/ox/ox-ms_abinger_c57.xml
 python tei-to-solr.py ../../../data/tei/ox/ox-ms_abinger_c58.xml
-python tei-to-solr.pt ../../../data/tei/bl/bl-loan_ms_70_08.xml
+python tei-to-solr.py ../../../data/tei/bl/bl-loan_ms_70_08.xml
